c193272278a16d544cd6a8470b4f6f8e9cc28a8e
Madhavi Mate <121250215+madhavimate@users.noreply.github.com>
2023-01-15 21:34:42 +0530

    Add files via upload

---
+++ routers/post.js
@@ -0,0 +1,459 @@
+const router = require('express').Router();
+const bodyParser = require('body-parser');
+const Post = require('./../models/post');
+const User = require('./../models/user');
+const Comment = require('./../models/comment');
+const passport = require('passport');
+const session = require('express-session');
+const passportLocalMongoose = require('passport-local-mongoose');
+const path = require('path');
+
+///File Upload
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads');
+    },
+    filename: (req, file, cb) => {
+       // console.log(file);
+        cb(null, file.fieldname + Date.now() + path.extname(file.originalname));
+    }
+});
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+const upload = multer({ storage: storage, fileFilter: fileFilter });
+
+
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended: true}));
+
+
+const LocalStrategy = require('passport-local').Strategy; 
+
+
+
+
+passport.use(new LocalStrategy(User.authenticate())); 
+router.use(session({
+    secret : process.env.PASS_SECRET,
+    resave :false,
+    saveUninitialized: false
+}));
+
+router.use(passport.initialize());
+router.use(passport.session());
+
+passport.serializeUser(User.serializeUser()); 
+passport.deserializeUser(User.deserializeUser()); 
+
+
+router.route("/")
+    .get(function(req,res){
+
+        if(!req.user){
+            res.redirect("/user/login");
+        }
+        else{
+           
+            Post.find().populate('author').exec(function(error,blogArray){
+                if(error){
+                    res.json({
+                        status:false,
+                        message:"error in rendering posts in /posts route" ,
+                        error:error
+                    })  
+                }
+                else if(blogArray.length==0){
+                    res.json({
+                        status: false,
+                        message:"No posts found, BlogArray is empty"
+                    });
+                }
+                else{
+                   // console.log("REQUSER IN /POSTS: ------->" + req.user._id);
+                    res.render('index',{blogArray:blogArray});
+                    // res.json({
+                    //     status: true,
+                    //     message: "All posts found",
+                    //     result: blogArray
+                    // })
+                }
+            })
+        }
+        
+    })
+    .post(function(req,res){
+        res.json({
+            status: false,
+            message: "Cannot give Post request to /posts"
+        })
+    })
+    .delete(function(req,res){
+        res.json({
+            status: false,
+            message: "Cannot give Delete request to /posts, All posts can't be deleted "
+        })
+    })
+    .put(function(req,res){
+        res.json({
+            status: false,
+            message: "Cannot give Put requests to /posts route"
+
+        })
+    })
+    .patch(function(req,res){
+        res.json({
+            status: false,
+            message: "Cannot give Patch requests to /posts route"
+        })
+    })
+
+
+
+router.route('/compose')
+    .get(function(req,res){
+        if(!req.user){
+            res.redirect("/user/login");
+        }
+        else{
+            res.render('compose');
+        }
+        
+    })
+    .post(upload.single('blogImage'), function(req,res){
+        if(!req.user){
+            res.redirect("/user/login");
+        }
+        else{
+            // console.log( req.file);
+            
+            var blogTitle = req.body.blogTitle;
+            var blogContent = req.body.blogContent;
+            
+            var newBlog = new Post({
+                textfield:{
+                    blogTitle:blogTitle,
+                    blogContent:blogContent
+                },
+                author: req.user._id,
+             })
+
+            if(req.file){
+                 newBlog = new Post({
+                    textfield:{
+                        blogTitle:blogTitle,
+                        blogContent:blogContent
+                    },
+                    author: req.user._id,
+                    image_id:req.file.filename
+                 })
+            }
+            
+            
+            console.log(newBlog);
+            newBlog.save(function(error, result){
+                if(error){
+                    res.json({
+                        status:false,
+                        message:"Post was not saved due to error..",
+                        error: error
+                    })
+                }
+                else{
+                    User.findByIdAndUpdate(req.user._id, {$push:{personal:newBlog._id}},function(error,result){
+                        if(error){
+                            res.json({
+                                status:false,
+                                message: "Blog created, but unable to push into personal array!",
+                                error: error
+                            })
+                        }
+                        else{
+                            res.redirect('/posts')
+                        }
+                    })   
+                }
+            })
+        }
+    })
+
+
+router.route('/:post_id')
+    .get(function(req,res){
+        if(!req.user){
+            res.redirect("/user/login");
+        }
+        else{
+            
+            Post.findOne({_id:req.params.post_id}).populate({
+                path:'comments',
+                populate:{
+                    path:'given_by'
+                }   
+            }).exec(function(error,result){
+                if(error){
+                    res.json({
+                        status:false,
+                        message:"Error in reading a particular post..."
+                    })
+                }
+                else if(result==null){
+                    res.json({
+                        status:false,
+                        message:"Post was not found..."
+                    })
+                }
+                else{
+                   res.render('readpost',{blogToRender:result, curr_user_id:req.user._id});
+                    
+                }
+            })  
+        }
+    })
+    ///////// REST API DELETE //////////////////////////////////
+    .delete( function(req,res){
+        if(!req.user){
+            res.json({
+                status:false,
+                message: "Authentication Error!, Please Login or Signup First!"
+            })
+        }
+        else{
+            //CHECK PERMISSIONS TO DELETE
+            Post.findOneAndDelete({_id: req.params.post_id, author:req.user._id}, function(error,result){
+                if(error){
+                    res.json({
+                        status: true,
+                        message: "Error in deleting Document!",
+                        error: error
+                    })
+                }
+                else if(result== null){
+                    res.json({
+                        status:false,
+                        message: "Permission Denied or No result found"    
+                    })
+                }
+                else{
+                    res.json({
+                        status: true,
+                        message: "Deleted Successfully"
+                    })
+                }
+            })
+        }
+    })
+    /// REST API PATCH/// 
+    .patch(function(req,res){
+        if(!req.user){
+            res.json({
+                status: false,
+                message: "Permission denied"
+            })
+        }
+        else{
+            if(req.body._id || req.body.google_id){
+                res.json({
+                    status: false,
+                    message: "IDs can't be changed, change only text fields"
+                })
+            }
+            else{
+                Post.update({_id:req.params.post_id}, {$set: req.body}, function(error, result){
+                    if(error){
+                        res.json({
+                            status: false,
+                            message: "Error in Updating document",
+                            error: error
+                        })
+                    }
+                    else if(result == null){
+                        res.json({
+                            status: false,
+                            message: "No post found with this Id",
+                            result: result
+                        })
+                    }
+                    else{
+                        res.json({
+                            status: true,
+                            message: "Post Updated",
+                            result: result
+                        })
+                    }
+                })
+            }
+        }
+    })
+    .put(function(req,res){
+        res.json({
+            status:false,
+            message: "Put is not supported! (It overwrites Comments section)"
+        })
+    })
+
+
+router.route('/:post_id/:image_id')
+    .get(function(req,res){
+        if(req.params.image_id == "DNE"){
+            res.json({
+                status: false,
+                message: "No Image is uploaded with this blog",
+            })
+        }
+        res.sendFile(path.join(__dirname , '../uploads/' + req.params.image_id))
+    })
+
+
+
+
+
+
+
+/////////////// Overriding Delete and Patch using post requests for webpage //////////////    
+
+router.route('/:post_id/delete')
+    .get(function(req,res){
+        if(!req.user){
+            res.redirect("/user/login");
+        }
+        else{
+            //CHECK PERMISSIONS TO DELETE
+            
+            Post.findOneAndDelete({_id: req.params.post_id, author:req.user._id}, function(error,result){
+                if(error){
+                    res.json({
+                        status: true,
+                        message: "Error in deletion!"
+                    })
+                }
+                
+                else if(result== null){
+                    res.json({
+                        status:false,
+                        message: "Permission Denied or No result found"
+
+                    })
+                }
+                
+                else{
+                    res.redirect("/posts");
+                }
+            })
+        }
+
+    })
+router.route('/:post_id/edit')
+    .get(function(req, res){
+        if(!req.user){
+            res.redirect("/user/login");
+        }
+        else{
+            
+            Post.findOne({_id: req.params.post_id, author:req.user._id},function(error ,result){
+                if(error){
+                    res.json({
+                        status:false,
+                        message: "Error in rendering edit page Or null"
+                    })
+                }
+                else if(result == null){
+                    res.json({
+                        status: false,
+                        message: "Permission Denied Or result Null"
+                    })
+                }
+                else{
+                   
+                    res.render('editAndReplace',{blogObject:result});
+                }
+            })
+        }
+    })
+    .post(function(req,res){
+        if(!req.user){
+            res.redirect("/user/login");
+        }
+        else{  
+            Post.updateOne({_id:req.params.post_id},{textfield:req.body}, function(error,result){
+                if(error){
+                    res.json({
+                        status: false,
+                        message: "Error in Updating document! or Null result "
+                    })
+                }
+                else{
+                    res.redirect('/posts')
+                }
+            })
+        }
+
+    })
+
+
+router.route('/:post_id/comment/add')
+    .post(function(req,res){
+        
+                            if(!req.user){
+                                res.redirect("/user/login")
+                            }
+                            else{    
+                            Post.findById(req.params.post_id,function(error, result){
+                                if(error){
+                                    res.json({
+                                        status: false,
+                                        message: " Error in finding Post to comment! "
+                                    })
+                                } 
+                                
+                                else{
+                                        const newComment = new Comment({ 
+                                            textfield: req.body.addComment,
+                                            given_by:req.user._id,
+                                            given_to: result.author._id
+                                        })
+                                        newComment.save(function(error,result1){
+                                            if(error){
+                                                res.json({
+                                                    status: false,
+                                                    message :"Error in saving the newComment!",
+                                                    error: error
+                                                })
+                                            }
+                                            else{
+
+                                                    
+                                                result.comments.push(newComment._id);
+                                                result.save(function (err,result){
+                                                    if(error){
+                                                        res.json({
+                                                            status:false,
+                                                            message: "Error in pushing comment to the specific Post!.."
+                                                        })
+                                                    }
+                                                });
+
+
+                                                var redirectPath = "/posts/" + req.params.post_id
+                                                res.redirect(redirectPath);
+                                            }
+                                        })
+
+                                    }
+                                })
+                            }
+        })       
+
+    
+                                
+
+          
+    
+module.exports = router
+
---
+++ routers/user.js
@@ -0,0 +1,265 @@
+const router = require('express').Router();
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const User = require('./../models/user');
+const Post = require('./../models/post');
+const Comment = require('./../models/comment');
+const passport = require('passport');
+const session = require('express-session');
+const passportLocalMongoose = require('passport-local-mongoose');
+const findOrCreate = require('mongoose-findorcreate')
+
+
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended: true}));
+
+
+const LocalStrategy = require('passport-local').Strategy; 
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+
+
+passport.use(new LocalStrategy(User.authenticate())); 
+router.use(session({
+    secret : process.env.PASS_SECRET,
+    resave :false,
+    saveUninitialized: false,
+}));
+
+router.use(passport.initialize());
+router.use(passport.session());
+
+passport.serializeUser(User.serializeUser()); 
+passport.deserializeUser(User.deserializeUser()); 
+
+// passport.use(new GoogleStrategy({
+//     clientID: process.env.CLIENT_ID,
+//     clientSecret: process.env.CLIENT_SECRET,
+//     callbackURL: "https://localhost:3000/user/auth/google/posts",
+//     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+//   },
+//   function(accessToken, refreshToken, profile, cb) {
+//     User.findOrCreate({ googleId: profile.id }, function (err, user) {
+//       return cb(err, user);
+//     });
+//   }
+// ));
+
+
+
+
+
+
+//------------------------------------------  CREATE ---------------------------------------------
+
+
+
+router.route("/")
+    .get(function(req,res){
+        User.find(function(error,result){
+            if(error){
+                res.json({
+                    status: false,
+                    message: "Error in finding Users",
+                    error: error
+                })
+            }
+            else{
+                res.json({
+                    status: true,
+                    message: "Users found Successfully",
+                    result: result
+                })
+            }
+        })
+    });
+    // .delete(function(req,res){
+    //     if(!req.user){
+    //         res.json({
+    //             status: false,
+    //             message: "Login required"
+    //         })
+    //     }
+    //     else()
+    // })
+
+
+router.route ("/register")
+    .get(function(req,res){
+        res.render('register');
+    })
+    .post(function(req,res){
+    const newUser = new User({
+        username:req.body.username,
+        email: req.body.email,
+        fullName: req.body.fullName
+    })
+    User.register(newUser, req.body.password, function(error,result){
+        if(error){
+            res.json({
+                status:false,
+                message: "Error in registering user",
+                error: error
+            })
+        }
+        else{
+            res.redirect('/user/login');
+        }
+    })
+})
+
+
+router.route ("/login",)
+    .get(function(req,res){
+        res.render('login');
+    })
+    .post(passport.authenticate('local'),function(req,res){
+        if(!req.body.username){
+         //   console.log("loggin in passed FIRST errors")
+            res.json({
+                status:false,
+                message: "No username provided"
+            })   
+        }
+        else{
+            if(!req.body.password){
+                res.json({
+                    status:false,
+                    message: "No password provided"
+                })
+            }
+            else{
+             //   console.log("basic errors checked");
+
+
+                passport.authenticate("local")(req, res, function() {
+                   res.redirect("/posts");
+                });
+
+            }
+        }
+    })
+
+
+
+router.route('/myposts')
+    .get(function(req,res){
+        if(!req.user){
+            res.redirect('/user/login')
+        }
+
+        else{
+            
+            User.findById(req.user._id).populate('personal').exec(function(error,result){
+                if(error){
+                    res.json({
+                        status :false,
+                        message: "Error in finding and populating"
+                    })
+
+                }
+                else{
+                    res.render('myblogs',{userObject:result})
+                }
+            })
+         }
+    })
+
+
+router.route('/profile')
+    .get(function(req,res){
+        if(req.user){
+            res.render('profile',{userObject:req.user});
+        }
+        else
+        {
+            res.redirect('/user/login');
+        }
+    })
+router.route('/logout')
+    .get(function(req,res){
+        req.logout();
+        res.redirect("/")
+    })
+
+//---------------------------------------- DELETE ----------------------------------------------------------------
+router.route('/delete')
+    .get(function(req,res){
+        User.deleteOne({_id:req.user._id},function(error,result){
+            if(error){
+                res.json({
+                    status:false,
+                    message:"Error in deleting the User",
+                    error:error
+                })
+            }
+            else{
+                Post.deleteMany({author: req.user._id}, function(error2,result2){
+                    if(error2){
+                        res.json({
+                            status: false,
+                            message: "Error in deleting users Posts"
+                        })
+                    }
+                    else{
+                        Comment.deleteMany({given_by:req.user._id}, function(error3,result3){
+                            if(error3){
+                                res.json({
+                                    status: false,
+                                    message: "Comments could not be found"
+                                })
+                            }
+                            else{
+                                res.json({
+                                    satus: true,
+                                    message: "User, Posts and Comments Deleted Successfully",
+                                    redirectLink: "http://localhost:3000/user/login"
+                                })
+                            }
+                        })
+                    }
+                })
+            }
+        })
+    })
+
+
+router.route('/edit')
+    .get(function(req,res){
+        if(!req.user){
+            res.json({
+                status: false,
+                message: " You must be signed in to Edit datails"
+
+            })
+        }
+        else{
+            res.render('profileEdit', {userObject: req.user})
+        }
+    })
+    .post(function(req,res){
+        if(!req.user){
+            res.json({
+                status: false,
+                message: "Sign in first"
+            })
+        }
+        else{
+            User.findByIdAndUpdate(req.user._id,{username:req.body.newUserName} ,function(error,result){
+                if(error){
+                    res.json({
+                        status: false,
+                        message: "Error in Updating User" ,
+                        error: error
+                    })
+                }
+                else{
+                    res.redirect("/user/profile")
+                    
+                }
+            })
+        }
+    })
+
+module.exports = router;
